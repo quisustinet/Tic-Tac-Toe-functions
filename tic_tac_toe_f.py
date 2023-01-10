3d83dfdbd8f3a171ce02e14be1a10b767613229d
Wojtek W <65029112+quisustinet@users.noreply.github.com>
2023-01-10 08:35:33 +0100

    Update tic_tac_toe_f.py

--- tic_tac_toe_f.py
+++ tic_tac_toe_f.py
@@ -1,4 +1,5 @@
 from random import randrange
+
     
 def display_board(board):
     # The function accepts one parameter containing the board's current status
@@ -18,29 +19,39 @@ def enter_move(board):
     # The function accepts the board's current status, asks the user about their move, 
     # checks the input, and updates the board according to the user's decision.
     # and prints it out to the console.
-    move=int(input("Enter your move:"))
-    if 0 < move < 10 and type(board[0][move-1])==int:
-        if move < 4 :
+    try:
+        move=int(input("Enter your move:"))
+    except:
+        print("Your move should be an integer number.")
+        return 0
+    if move < 1:
+        print("You can't do this move. Try again.")
+        enter_move(board)
+    elif move < 4:
+        if type(board[0][move-1])==int:
             board[0][move-1]="O"
-        elif move < 7 :
+    elif move < 7:
+        if type(board[1][move-3-1])==int:
             board[1][move-3-1]="O"
-        elif move < 10 :
+    elif move < 10:
+        if type(board[2][move-6-1])==int:
             board[2][move-6-1]="O"
     else:
         print("You can't do this move. Try again.")
-        enter_move(board)
+       
 
 def make_list_of_free_fields(board):
     # The function browses the board and builds a list of all the free squares; 
     # the list consists of tuples, while each tuple is a pair of row and column numbers.
     # and prints it out to the console.
+    global free_fields
+    free_fields=[]
     for i in board:
         for j in i:
             s=(board.index(i),i.index(j))
             if type(j)==int and s not in free_fields:
                 free_fields.append(s)
-            #else:
-                #free_fields.remove(s)
+         
                 
     
 
@@ -55,23 +66,24 @@ def draw_move(board):
     # and prints it out to the console.
     return 0
 
-
-board=[]
-free_fields=[]
-n=1
-for i in range(3):
-    row=[n+i for i in range(3)]
-    board.append(row)
-    n+=3
+def create_board():
+    global board
+    board=[]
+    n=1
+    for i in range(3):
+        row=[n+i for i in range(3)]
+        board.append(row)
+        n+=3
+        
+create_board()
 board[1][1]='X'
-print(board)
 display_board(board)
-make_list_of_free_fields(board)
-print(free_fields)
+print(board)
 while True:
     enter_move(board)
+    make_list_of_free_fields(board)
     display_board(board)
     print(board)
-    make_list_of_free_fields(board)
     print(free_fields)
+
 
